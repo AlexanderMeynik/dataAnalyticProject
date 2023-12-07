4540fdf90e1f3f8876653d1ad2ab28dbff8b757b
AlecadM <71269820+AlexanderMeynik@users.noreply.github.com>
2023-12-07 15:31:37 +0300

    Delete redundant files

--- clientCode/pages/home.py
+++ clientCode/pages/home.py
@@ -4,6 +4,6 @@ from dash import html
 dash.register_page(__name__, path='/')
 
 layout = html.Div([
-    html.H1('Это базовая страница'),
+    html.H1('Это базовая страница дэшборда'),
     html.Div('Здесь ничего не будет'),
 ])
